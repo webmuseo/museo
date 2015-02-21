8049065be6688d6a75efede869d58b8a74aa896f
universidad <universidad@PORTALUM2>
2015-02-21 17:50:33 +0100

    Commit dropzone

---
+++ app/database/seeds/CarpetasTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+class CarpetasTableSeeder extends Seeder {
+	public function run() {
+		DB::table ( 'carpetas' )->delete ();
+		
+		$carpetas = array (
+				array ( // 1
+						'nombre'=> 'Default',
+				)
+				
+		)
+		;
+		
+		DB::table ( 'carpetas' )->insert ( $carpetas );
+		
+	}
+}
